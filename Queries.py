34dc9fbe265adf60149d7ede55fb54fffa76b0ea
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2022-08-04 21:34:59 +0400

    Implementation of some ideas

--- Queries.py
+++ Queries.py
@@ -1,6 +1,11 @@
-import requests, json, constants
+import requests, json, constants, time, random
 
 class Queries:
+    
+    def random_sleep_timer(self) -> None:
+        timer: int = random.randint(3, 10)
+        print(f"Sleeping for {timer} seconds")
+        time.sleep(timer)
     
     # Get the id of the instagram user
     def get_user_id(self, username: str, auth_session: requests.Session) -> str:
@@ -18,15 +23,17 @@ class Queries:
         edges = []
         if request.status_code == 200:
             json_data = json.loads(request.text)
-            #has_next_page = json_data["data"]["user"]["edge_follow"]["page_info"]["has_next_page"]
+            has_next_page = json_data["data"]["user"]["edge_followed_by"]["page_info"]["has_next_page"]
+            numbers_of_followers = json_data["data"]["user"]["edge_followed_by"]["count"]
             edges.append(json_data["data"]["user"]["edge_followed_by"]["edges"])
             """ while has_next_page:
-                end_cursor = json_data["data"]["user"]["page_info"]["end_cursor"]
+                end_cursor = json_data["data"]["user"]["edge_followed_by"]["page_info"]["end_cursor"]
                 url_to_request = constants.BASED_URL + f"graphql/query/?query_id=17851374694183129&id={user_id}&first=10&after={end_cursor}"
+                self.random_sleep_timer()
                 request = auth_session.get(url_to_request)
                 if request.status_code == 200:
                     json_data = json.loads(request.text)
-                    has_next_page = json_data["data"]["user"]["page_info"]["has_next_page"]
+                    has_next_page = json_data["data"]["user"]["edge_followed_by"]["page_info"]["has_next_page"]
                     edges.append(json_data["data"]["user"]["edge_followed_by"]["edges"])
             edges.append(json_data["data"]["user"]["edge_followed_by"]["edges"]) """
             return edges
@@ -40,6 +47,7 @@ class Queries:
         edges = []
         if request.status_code == 200:
             json_data = json.loads(request.text)
+            print(json_data)
             has_next_page = json_data["data"]["user"]["edge_follow"]["page_info"]["has_next_page"]
             edges.append(json_data["data"]["user"]["edge_follow"]["edges"])
             """ while has_next_page:
@@ -54,3 +62,8 @@ class Queries:
             return edges
         else:
             return None
+    
+    def get_followers_following_data(self, follow_list: list[list]):
+        for edge in follow_list:
+            for follower in edge:
+                print(follower)
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from Queries import Queries
 from SessionProcessing import SessionProcessing
+import pickle, os
+
+def follow_data_processing(follow_data: list) -> list:
+    for edge in follow_data:
+        for follow in edge:
+            print("{:<20}{:>20}".format(follow["node"]["id"], follow["node"]["username"]))
 
 def main() -> None:
     session_processing = SessionProcessing()
@@ -8,7 +14,35 @@ def main() -> None:
     settings = session_processing.load_settings()
     session = session_processing.auth_session(settings)
     user_id = queries.get_user_id(settings["username_target_account"], session)
-    print(queries.get_following_list(user_id, session))
+    
+    if not os.path.exists("data/followers_data.pickle"):
+        print("No followers data found. Generating...")
+        followers_data = queries.get_followers_list(user_id, session)
+        if not os.path.exists("data"):
+            os.mkdir("data")
+        with open("data/followers_data.pickle", 'wb') as f:
+            pickle.dump(followers_data, f)
+    else:
+        print("Followers data found. Loading...")
+        with open("data/followers_data.pickle", 'rb') as f:
+            followers_data = pickle.load(f)
+    
+    
+    if not os.path.exists("data/following_data.pickle"):
+        print("No following data found. Generating...")
+        following_data = queries.get_following_list(user_id, session)
+        if not os.path.exists("data"):
+            os.mkdir("data")
+        with open("data/following_data.pickle", 'wb') as f:
+            pickle.dump(following_data, f)
+    else:
+        print("Following data found. Loading...")
+        with open("data/following_data.pickle", 'rb') as f:
+            following_data = pickle.load(f)
+    
+    follow_data_processing(followers_data)
+    print("\n")
+    follow_data_processing(following_data)
 
 if __name__ == "__main__":
     main()
